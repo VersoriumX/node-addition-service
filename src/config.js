c886c58418908f38a85b8edf5e033e75caede02c
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-02-22 00:15:46 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// config.js
+const config = {
+    metalsApiKey: 'YOUR_METALS_API_KEY',
+    cryptoApiKey: 'YOUR_CRYPTO_API_KEY',
+    // Add other configuration settings as needed
+};
+
+module.exports = config;
